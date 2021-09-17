5e79e36a06bd1416edc5e0b18cc0eed6b65d2b05
Jérémy Leroy <jeremy-leroy@live.fr>
2021-09-17 18:07:51 +0200

    modif affichage img

--- monprgm.py
+++ monprgm.py
@@ -135,9 +135,6 @@ def main(threadName, *args): # *args correspond a un tuple qui peut donc conteni
             continuer = True
             while continuer:
                 ecran.blit(image, (0,0))
-                for event in pygame.event.get():
-                    if event.type == pygame.KEYDOWN:
-                        continuer = False
                 pygame.display.flip()
             pygame.quit()
             WaitForEvent() #Puis on lance (arriere plan) la fonction qui bloque la fonction tant qu'il n'y a pas d'appui

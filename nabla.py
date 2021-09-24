baf3d37246468abfd5dd69e354e2f344d748ae55
JeremyL <47211519+TheJerem76@users.noreply.github.com>
2021-09-24 09:05:36 +0200

    indentation error ligne 76 et 77

--- nabla.py
+++ nabla.py
@@ -73,8 +73,8 @@ def AfficherTexteAccueil(message): # Afficher un Texte sur l'image d'accueil (ou
 
 
 if (os.path.isdir("/home/pi/Desktop/photos") == False): # si le dossier pour stocker les photos n'existe pas       
-   os.mkdir("/home/pi/Desktop/photos")                  # alors on crée le dossier (sur le bureau)
-   os.chmod("/home/pi/Desktop/photos",0o777)            # et on change les droits pour pouvoir effacer des photos
+    os.mkdir("/home/pi/Desktop/photos")                  # alors on crée le dossier (sur le bureau)
+    os.chmod("/home/pi/Desktop/photos",0o777)            # et on change les droits pour pouvoir effacer des photos
 
     AfficherPhoto("/home/pi/Photobooth/images/appuyezbouton.jpg")
 #AfficherTexteAccueil("Installez-vous et appuyez sur le bouton pour prendre une photo")
